7dc28343720b0b1249b37489b19455f2500d133d
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-12 15:03:19 +0530

    [Pallavi Athawale Add: file]

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakeandLadder
+{
+    internal class Program
+    {
+       public const int START_POINT = 0;
+       public const int END_POINT = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snakes and Ladder Game \nEnter Player Name");
+            string player1 = Console.ReadLine();
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
+        }
+    }
+    
+}
